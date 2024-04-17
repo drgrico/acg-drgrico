26bae008eba5bdf9463c849a083104ff49929cb5
drgrico <riccardodrago0918@gmail.com>
2024-04-17 13:28:52 +0900

    task01 finished

--- task01/main.cpp
+++ task01/main.cpp
@@ -65,7 +65,19 @@ void draw_polygon(
         float p1x = polygon_xy[i1_vtx * 2 + 0] - x;
         float p1y = polygon_xy[i1_vtx * 2 + 1] - y;
         // write a few lines of code to compute winding number (hint: use atan2)
+
+        float magn0 = sqrt(p0x*p0x + p0y*p0y);
+        float magn1 = sqrt(p1x*p1x + p1y*p1y);
+        float magn_prod = magn0*magn1;
+ 
+        float cos_theta = (p0x*p1x + p0y*p1y) / magn_prod;
+        float sin_theta = (p1x*p0y - p0x*p1y) / magn_prod;
+
+        winding_number += atan2(sin_theta, cos_theta);        
       }
+      
+      winding_number /= (2*M_PI);
+      
       const int int_winding_number = int(std::round(winding_number));
       if (int_winding_number == 1 ) { // if (x,y) is inside the polygon
         img_data[ih*width + iw] = brightness;
@@ -91,6 +103,20 @@ void dda_line(
   auto dx = x1 - x0;
   auto dy = y1 - y0;
   // write some code below to paint pixel on the line with color `brightness`
+  float m = dy / dx;
+  int i = 0;
+
+  if (std::abs(dy) < std::abs(dx)) { 
+    for (int x = std::min(x0, x1); x <= std::max(x0, x1); x++) {
+      int y = y0 + m * (x - x0);
+      img_data[y * width + x] = brightness;
+    }
+  } else { 
+    for (int y = std::min(y0, y1); y <= std::max(y0, y1); y++) {
+      int x = x0 + (y - y0) / m;
+      img_data[y * width + x] = brightness;
+    }
+  }
 }
 
 int main() {
